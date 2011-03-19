d37f3304b66049f55a1aaadae10bad57a0716560
unknown <Debayan Gupta@.(none)>
2011-03-19 02:17:39 -0400

    Simple linked selection (removed separate focus+zoom experiment)

--- html/mainscript.js
+++ html/mainscript.js
@@ -1,6 +1,6 @@
 var vis = [];
 var p = [];
-var dx;
+var dx = [];
 
 function _gel(x) {return document.getElementById(x)}
 
@@ -21,32 +21,45 @@ function doAJAX(target) {
 
 function getData( plotType, dataLoc, width, height) { // minimum args = 4
 
-	
+
 	this.w = width;
 	this.h = height;
-	
+
 	this.data = JSON.parse( doAJAX( dataLoc ) );
-	
+
 	switch( plotType ) {
-	
+
 	case 'scatterplot':
 		// scatterplot requires 3 more arguments: the names of the x,y,z fields
 		var vx = arguments[4]||'x', vy = arguments[5]||'y', vz = arguments[6]||'z';
-		this.xval 	= 	new Function("d", "return this.root.x(d." + vx + ")");
-		this.yval 	= 	new Function("d", "return this.root.y(d." + vy + ")");
-		this.colour = 	new Function("d", "return this.root.c(d." + vz + ")");
-		this.size 	= 	new Function("d", "return d." + vz);
-		this.title 	= 	new Function("d", "return d." + vz + ".toFixed(1)");
+		this.xval 	= new Function("d", "return this.root.x(d." + vx + ")");
+		this.yval 	= new Function("d", "return this.root.y(d." + vy + ")");
+		this.colour = new Function("d", "return this.root.c(d." + vz + ")");
+		this.size 	= new Function("d", "return d." + vz);
+		this.title 	= new Function("d", "return d." + vz + ".toFixed(1)");
 		break;
-		
+
+	case 'focus+zoom':
+		//focus+zoom requires 3 more arguments: height of small graph, and names of x,y fields
+		this.h2 = arguments[4];
+
+		var vx = arguments[5]||'x', vy = arguments[6]||'y';
+		this.x		= new Function("d", "return d." + vx);
+		this.y		= new Function("d", "return d." + vy);
+		this.xval	= new Function("d", "return this.root.x(d." + vx + ")");
+		this.yval	= new Function("d", "return this.root.y(d." + vy + ")");
+		this.fx		= new Function("d", "return this.root.fx(d." + vx + ")");
+		this.fy		= new Function("d", "return this.root.fy(d." + vy + ")");
+		break;
+
 	default:
 		alert('What the heck is a "' + plotType + '" ?!');
 	}
-	
+
 	return this;
 }
 
-function doUpdate( command ) {
+function doDataUpdate( command ) {
 
 	var response = JSON.parse( doAJAX( command ) );
 	
@@ -58,4 +71,8 @@ function doUpdate( command ) {
 		vis[ u[i] ].s = false; //removes earlier selection
 		vis[ u[i] ].render();
 	}
+}
+
+function doSelection() {
+	
 }
